692105280532d0901970e1f8515d43eb6f944c63
AlonLivne <livnealon@gmail.com>
2018-11-06 15:35:47 +0200

    FirstCommit

--- Dualismus/Assets/Scripts/Board.cs
+++ Dualismus/Assets/Scripts/Board.cs
@@ -4,13 +4,68 @@ using UnityEngine;
 
 public class Board : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    int BoardSizeX;
+    int BoardSizeY;
+    public static Board Singleton;
+
+    public Tile[,] AllTiles;
+
+    public Tile TilePrefab;
+
+    private void Awake()
+    {
+        Singleton = this;
+        AllTiles = new Tile[BoardSizeX , BoardSizeY];
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
+    public static bool IsIndexInBoard(int x, int y)
+    {
+        if (x < 0 || y < 0 || y >= Singleton.BoardSizeY || x >= Singleton.BoardSizeY)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void SetUpBoard()
+    {
+        for (int i = 0; i<BoardSizeX; i++)
+        {
+            for (int j = 0; j < BoardSizeY; j++)
+            {
+                AllTiles[i, j] = Instantiate(TilePrefab, this.transform);
+                AllTiles[i, j].Init(i, j, TileType.Clear);
+            }
+        }
+    }
+
+    public void EndTurn()
+    {
+        var treeTiles = new List<Tile>();
+        foreach (var tile in AllTiles)
+        {
+            if (tile.TileData.Tiletype == TileType.Tree)
+            {
+                treeTiles.Add(tile);
+            }
+        }
+
+        foreach (var tree in treeTiles)
+        {
+            tree.PlayTurn();
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
---
+++ Dualismus/Assets/Scripts/Tile.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType
+{
+    Clear,
+    Tree,
+    Building,
+    CutTree
+}
+
+[Serializable]
+public class TileData
+{
+    public Vector2Int Coordinates;
+    public TileType Tiletype;
+}
+
+public class Tile : MonoBehaviour {
+    public TileData TileData;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Init(int x, int y, TileType type)
+    {
+        TileData.Coordinates = new Vector2Int(x, y);
+        ChangeTileType(type);
+    }
+
+    public void PlayTurn()
+    {
+        switch (TileData.Tiletype)
+        {
+            case (TileType.Tree):
+                TryExpanding();
+                break;
+            default:
+                return;              
+        }
+    }
+
+    public void TryExpanding()
+    {
+        int _destinationX = TileData.Coordinates.x + UnityEngine.Random.Range(-1, 1);
+        int _destinationY = TileData.Coordinates.y + UnityEngine.Random.Range(-1, 1);
+
+        if (Board.IsIndexInBoard(_destinationX,_destinationY))
+        {
+            FailGrow();
+        }
+
+        Board.Singleton.AllTiles[_destinationX, _destinationY].TryGrowTree();
+
+    }
+
+    public void FailGrow()
+    {
+        //ToDo - insert failing animation
+    }
+
+    public bool TryGrowTree()
+    {
+        for (int i = -1; i < 2; i++)
+        {
+            for (int j = -1; j < 2; j++)
+            {
+                if (Board.IsIndexInBoard(i, j))
+                {
+                    continue;
+                }
+
+                if (Board.Singleton.AllTiles[i,j].TileData.Tiletype == TileType.Building)
+                {
+                    FailGrow();
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public void ChangeTileType(TileType newType)
+    {
+        TileData.Tiletype = newType;
+    }
+    
+}
